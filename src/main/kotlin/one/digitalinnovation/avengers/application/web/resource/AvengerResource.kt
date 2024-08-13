17bc9ac779417765e56e7396930f43cd441aa215
Diego Barcellos <diegocb81@gmail.com>
2024-08-13 19:41:45 -0300

    Created profiles

--- src/main/kotlin/one/digitalinnovation/avengers/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/one/digitalinnovation/avengers/application/web/resource/AvengerResource.kt
@@ -6,6 +6,7 @@ import one.digitalinnovation.avengers.application.web.resource.response.AvengerR
 import one.digitalinnovation.avengers.domain.avenger.AvengerRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -30,7 +31,7 @@ class AvengerResource(
         }
 
 
-    @GetMapping("{id}")
+    @GetMapping("{id}/detail")
     fun getAvengerDetails(@PathVariable("id") id: Long) =
         repository.getDetail(id)?.let {
             ResponseEntity.ok().body(AvengerResponse.from(it))
@@ -55,5 +56,11 @@ class AvengerResource(
                 ResponseEntity.accepted().body(AvengerResponse.from(avenger))
             }
         } ?: ResponseEntity.notFound().build<Void>()
+
+    @DeleteMapping("{id}")
+    fun deleteAvenger(@PathVariable("id") id: Long) =
+        repository.delete(id).let {
+            ResponseEntity.accepted().build<Void>()
+        }
 
 }
